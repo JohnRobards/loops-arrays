c8e05e8b4489f6644b27ef2a0e9dd4088577791b
JohnRobards <jrobards@wustl.edu>
2016-04-25 18:02:37 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,3 +10,7 @@ while (count < 11) {
 // output results
 document.write("<h1>Loops</h1>");
 document.write("<ol>" + list + "</ol>");
+
+var arr = { "q1", "q2" };
+
+document.write("<p>" + arr + "</p>")
