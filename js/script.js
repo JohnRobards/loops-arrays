7ae3d0e185f4d30dd81499bd66b2d78db9057443
JohnRobards <jrobards@wustl.edu>
2016-04-25 18:06:16 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,9 +14,11 @@ document.write("<ol>" + list + "</ol>");
 var arr = { "q1", "q2", "q3", "q4" };
 
 var secondList = "<ul>"; 
+var i = 0;
 
 while (i < arr.length) {
   secondList +=("<li>" + arr[i] + "</li>");
+  i++;
 }
 secondList += "</ul>";
 document.write(secondList);
